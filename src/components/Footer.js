a42ced6d8c18be6cfe16d817bb02294476cc132d
Juan Gustavo Quinones <98677463+Swus1978@users.noreply.github.com>
2025-04-30 14:02:04 -0500

    Added more images updated button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,20 @@
+import Order from "./Order";
+
 export default function Footer() {
   const hour = new Date().getHours();
   const openHour = 12;
   const closeHour = 22;
-  const isOpen = hour >= openHour && hour <= closeHour;
-
-  console.log(isOpen);
-
-  //   if (hour >= openHour && hour <= closeHour) {
-  //     alert("We are currently open!");
-  //   } else {
-  //     alert("Sorry, we are closed.");
-  //   }
+  const isOpen = hour >= openHour && hour < closeHour;
 
   return (
     <footer className="footer">
-      <p>{new Date().toLocaleTimeString()}. We're currently open!</p>
+      {isOpen ? (
+        <Order closeHour={closeHour} />
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
+      )}
     </footer>
   );
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,10 +1,25 @@
 import Pizza from "./Pizza";
+import pizzaData from "../data/pizzadata"; // adjust the path as needed
 
 export default function Menu() {
+  const pizzas = pizzaData;
+  // const pizzas = [];
+  const numpizzas = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza
+      {numpizzas > 0 ? (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => {
+            return <Pizza pizzaObj={pizza} key={pizza.name} />;
+          })}
+        </ul>
+      ) : (
+        <p>We're still working on our menu. Please come back later!</p>
+      )}
+
+      {/* <Pizza
         photoName="images/spinaci.jpg"
         name="Pizza Spinaci"
         ingredients="Tomato, mozarella, spinach, olive, basil"
@@ -15,7 +30,7 @@ export default function Menu() {
         name="Pizza Funghi"
         ingredients="Tomato, mozarella, mushrooms, and onion"
         price={12}
-      />
+      /> */}
     </main>
   );
 }
